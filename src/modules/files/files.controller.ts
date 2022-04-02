e2cebb72879f407f03fdfa7a46a79f9b0d2a96a3
rubenphi <rubenphii@gmail.com>
2022-04-01 23:01:10 -0500

    se valida si el archivo existe antes de mostrarlo

--- src/modules/files/files.controller.ts
+++ src/modules/files/files.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get, StreamableFile, Param } from '@nestjs/common';
-import { createReadStream } from 'fs';
+import { Controller, Get, StreamableFile, Param, NotFoundException } from '@nestjs/common';
+import { createReadStream, existsSync } from 'fs';
 import { join } from 'path';
 
 @Controller('files')
 export class FilesController {
 	@Get('uploads/:name')
-	getFile(@Param('name') name: string): StreamableFile {
-		const file = createReadStream(join(process.cwd(), 'uploads/' + name));
-		return new StreamableFile(file);
+	getFile(@Param('name') name: string): StreamableFile | string {
+		if (existsSync('uploads/' + name)) { 
+			const file = createReadStream(join(process.cwd(), 'uploads/' + name));
+			return new StreamableFile(file);
+		  } else {
+			throw new NotFoundException(`El archivo llamado ${name} no existe`);
+		  }
+		
+		
+
 	}
 }
