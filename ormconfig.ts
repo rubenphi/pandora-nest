2f1a850d6cd7eb669775db6b9bdbdd2237c4e567
rubenphi <rubenphii@gmail.com>
2022-05-29 17:48:08 -0500

    Login y token sin roles 2

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,4 +1,3 @@
-import {TypeOrmModuleOptions} from '@nestjs/typeorm'
 import { DataSource } from 'typeorm';
 import * as dotenv from 'dotenv';
 import { join } from 'path';
@@ -6,15 +5,15 @@ import { join } from 'path';
 dotenv.config();
 
 const source = new DataSource({
-    type: process.env.DB_TYPE as any,
-    host: process.env.DB_HOST,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    dropSchema: false,
-    synchronize: true,
-    entities: [join(__dirname, "**","**", "*.entity.{ts,js}")],
-    migrations: [join(__dirname, "database","migrations", "*.{ts,js}")]
-  })
+	type: process.env.DB_TYPE as any,
+	host: process.env.DB_HOST,
+	username: process.env.PGUSER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.PGDATABASE,
+	dropSchema: false,
+	synchronize: true,
+	entities: [join(__dirname, '**', '**', '*.entity.{ts,js}')],
+	migrations: [join(__dirname, 'database', 'migrations', '*.{ts,js}')],
+});
 
 export default source;
