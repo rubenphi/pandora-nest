4c9adc5e28f869305034ef6ac4815af90c3b12f3
rubenphi <rubenphii@gmail.com>
2022-06-20 21:53:16 -0500

    Se añaden más descripciones a la documentación

--- src/modules/answers/dto/create-answer.dto.ts
+++ src/modules/answers/dto/create-answer.dto.ts
@@ -1,15 +1,35 @@
-import { IsInt, IsBoolean } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsInt, IsBoolean, IsNumber } from 'class-validator';
 
 export class CreateAnswerDto {
-
+	@ApiProperty({
+		description: 'Option´s id of answer',
+	})
 	@IsInt()
 	readonly optionId: number;
+	@ApiProperty({
+		description: 'Question´s id of answer',
+	})
 	@IsInt()
 	readonly questionId: number;
+	@ApiProperty({
+		description: 'Group´s id of answer',
+	})
 	@IsInt()
 	readonly groupId: number;
+	@ApiProperty({
+		description: 'Lesson´s id of answer',
+	})
 	@IsInt()
 	readonly lessonId: number;
+	@ApiProperty({
+		description: 'Points of answer',
+	})
+	@IsNumber()
+	readonly points: number;
+	@ApiProperty({
+		description: 'Indicates if the course is active or "deleted"',
+	})
 	@IsBoolean()
 	readonly exist: boolean;
 }
