829aabb15cb44fa8344e2c085426594539c3bd9b
rubenphi <rubenphii@gmail.com>
2023-08-10 01:12:30 +0000

    fix: error usuario

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -5,9 +5,10 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { User } from './user.entity';
 import { Institute } from '../institutes/institute.entity';
+import { Invitation } from '../invitations/invitation.entity';
 
 @Module({
-	imports: [TypeOrmModule.forFeature([User, Institute])],
+	imports: [TypeOrmModule.forFeature([User, Institute, Invitation])],
 	providers: [UsersService],
 	controllers: [UsersController],
 	exports: [UsersService],
